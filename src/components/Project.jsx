b41b7df6a226b9bc3b2809e65dc1882f354133b4
Atoti2 <bogyikormi3@gmail.com>
2024-02-22 15:52:55 +0100

    can't open 2 code pieces at once

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,9 @@
 import { useState} from 'react'
 import logo from '../assets/github-mark.png'
-
+import {motion} from "framer-motion"
 const Project = ({info}) => {
     const {name, descr, mainImg, gitHub, hosted, images } = info
     const [open, setOpen] = useState(false)
-    const [num, setNum] = useState(0)
     return(
         <>
         <div className='flex flex-col bg-gradient-to-tr shadow-xl from-slate-100 to-slate-200 rounded-md '>
@@ -15,17 +14,24 @@ const Project = ({info}) => {
             </div>
             <div className='flex'>
                 <a target='_blank' rel='noreferrer' className='w-fit' href={gitHub}><img className='h-8 mb-10 ml-10 hover:scale-105' src={logo} alt="Github page" /></a>
-                
+        
                 <svg onClick={() => setOpen(!open)} className='h-8 mb-10 ml-10 hover:scale-105 cursor-pointer' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>code part</title><path d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" /></svg>
-
             </div>
         </div>
+       {open && (
+            <motion.div
+            initial={{opacity: 0}}
+            animate= {{opacity: 1}}
+            exit={{opacity: 0}}
+             className='fixed top-0 left-0 w-screen h-screen bg-black/50'>
+            <div className='fixed top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2 z-10 p-10 rounded-md'>
+                <img className='rounded-md' src={images} alt="" />
+                <button className='text-3xl cursor-pointer text-white absolute top-10 font-semibold right-12' onClick={() => setOpen(!open)}>X</button>
+            </div>
+            </motion.div>
+       )}
+      
         
-        <dialog open={open} className='z-10 p-10 rounded-md bg-rose-600 backdrop:bg-gray-50'>
-            <img className='rounded-md' src={images} alt="" />
-            <button className='text-2xl cursor-pointer' onClick={() => setOpen(!open)}>Close</button>
-          
-        </dialog>
         
        
         </>
