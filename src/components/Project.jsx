0dcf76fd89ccad3e9544798761b6154440e398a2
Atoti2 <bogyikormi3@gmail.com>
2024-02-24 21:01:44 +0100

    link animation

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -18,6 +18,7 @@ const Project = ({info}) => {
                 <svg onClick={() => setOpen(!open)} className='h-8 mb-10 ml-10 hover:scale-105 cursor-pointer' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>code part</title><path d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" /></svg>
             </div>
         </div>
+        
        {open && (
             <motion.div
             initial={{opacity: 0}}
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react"
 import { NavLink, Outlet } from "react-router-dom"
-
+import {motion} from "framer-motion"
+import { AnimatePresence } from 'framer-motion'
 const RootLayout = () => {
 
     const [open, setOpen] = useState(false)
-    
+    const links = [
+      {
+        link : '/',
+        title: 'Kezdőlap',
+      },
+      {
+        link : 'about',
+        title: 'Rólam',
+      },
+      {
+        link : 'project',
+        title: 'Projektek',
+      },
+      {
+        link : 'contact',
+        title: 'Elérhetőség',
+      }
 
+    ]
   
     return (
         <>
@@ -21,22 +39,40 @@ const RootLayout = () => {
             </ul>
             <div className="flex flex-col md:hidden visible gap-5">
               {!open ? (
-                <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpen(!open)} className="w-12 h-12 hover:rotate-2 cursor-pointer transition-all" viewBox="0 0 24 24"><title>menu-open</title><path d="M21,15.61L19.59,17L14.58,12L19.59,7L21,8.39L17.44,12L21,15.61M3,6H16V8H3V6M3,13V11H13V13H3M3,18V16H16V18H3Z" /></svg>
+                <div
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpen(!open)} className="w-12 h-12 hover:rotate-2 cursor-pointer transition-all" viewBox="0 0 24 24"><title>menu-open</title><path d="M21,15.61L19.59,17L14.58,12L19.59,7L21,8.39L17.44,12L21,15.61M3,6H16V8H3V6M3,13V11H13V13H3M3,18V16H16V18H3Z" /></svg>
+                </div>
                 )
                 :
                 (
-                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpen(!open)} className="w-12 h-12 hover:rotate-2 cursor-pointer transition-all" viewBox="0 0 24 24"><title>menu-close</title><path d="M3 6H13V8H3V6M3 16H13V18H3V16M3 11H15V13H3V11M16 7L14.58 8.39L18.14 12L14.58 15.61L16 17L21 12L16 7Z" /></svg>
+                  <div
+               
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpen(!open)} className="w-12 h-12 hover:rotate-2 cursor-pointer transition-all" viewBox="0 0 24 24"><title>menu-close</title><path d="M3 6H13V8H3V6M3 16H13V18H3V16M3 11H15V13H3V11M16 7L14.58 8.39L18.14 12L14.58 15.61L16 17L21 12L16 7Z" /></svg>
+
+                  </div>
                 )
               }
+              <AnimatePresence>
               {open && (
-              <ul>
-                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.55s_ease-in-out]'><NavLink to="/">&#60;Kezdőlap/&#62;</NavLink></li>
-                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.66s_ease-in-out]'><NavLink to="about">&#60;Rólam/&#62;</NavLink></li>
-                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.77s_ease-in-out]'><NavLink to="project">&#60;Projektek/&#62;</NavLink></li>
-                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.88s_ease-in-out]'><NavLink to="contact">&#60;Elérhetőség/&#62;</NavLink></li>
-              </ul>)
-              }        
-
+                
+                <ul className="fixed top-24 backdrop-blur-md rounded-md">
+                  {links.map((item, index) => {
+                    return(
+                      <motion.li
+                      key={index}
+                      initial={{translateX: -45}}
+                      animate= {{translateX: 0}}
+                      exit={{translateX: -45}}
+                      transition={{duration: `0.0${9 + index}`}}
+                      onClick={() => setOpen(!open)} className='navTag'><NavLink to={item.link}>&#60;{item.title}/&#62;</NavLink></motion.li>
+                    )
+                  })}
+              </ul>
+            )
+          }        
+            </AnimatePresence>
             </div>
           </div>
           
