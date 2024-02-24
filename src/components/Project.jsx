dea99d74e3cf49f1e1275718db013805bdcc8d4b
Atoti2 <bogyikormi3@gmail.com>
2024-02-24 13:10:17 +0100

    image view

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,12 +9,12 @@ const Project = ({info}) => {
         <div className='flex flex-col bg-gradient-to-tr shadow-xl from-slate-100 to-slate-200 rounded-md '>
             <div className="p-8">
                 <h3 className="text-xl font-bold text-center mb-5">&#60;{name}/&#62;</h3>
-                <img className="transition-all cursor-pointer hover:scale-105 bg-center rounded-md h-auto  w-fit m-auto" src={mainImg} alt={name} onClick={() => window.open(hosted)}/>
+                <img className="transition-all cursor-pointer hover:scale-105 bg-center rounded-md h-auto w-full  m-auto" src={mainImg} alt={name} onClick={() => window.open(hosted)}/>
                 <p className="text-center mt-3 text-lg">{descr}</p>
             </div>
             <div className='flex'>
                 <a target='_blank' rel='noreferrer' className='w-fit' href={gitHub}><img className='h-8 mb-10 ml-10 hover:scale-105' src={logo} alt="Github page" /></a>
-        
+                
                 <svg onClick={() => setOpen(!open)} className='h-8 mb-10 ml-10 hover:scale-105 cursor-pointer' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>code part</title><path d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" /></svg>
             </div>
         </div>
@@ -23,17 +23,18 @@ const Project = ({info}) => {
             initial={{opacity: 0}}
             animate= {{opacity: 1}}
             exit={{opacity: 0}}
-             className='fixed top-0 left-0 w-screen h-screen bg-black/50'>
-            <div className='fixed top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2 z-10 p-10 rounded-md'>
-                <img className='rounded-md' src={images} alt="" />
-                <button className='text-3xl cursor-pointer text-white absolute top-10 font-semibold right-12' onClick={() => setOpen(!open)}>X</button>
-            </div>
+             className='fixed top-0 left-0 w-screen h-scree'>
+                <div className="flex flex-col justify-center items-center min-h-screen bg-black/70">
+                    <img className='rounded-md hover:scale-105' onClick={(()  => {
+                        window.open(images) 
+                        setOpen(!open)
+                        } )}  src={images} alt="" />
+                    <button className='text-3xl cursor-pointer text-white font-semibold right-12' onClick={() => setOpen(!open)}>Close</button>
+                </div>
             </motion.div>
        )}
-      
         
         
-       
         </>
     )
 }
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,11 +4,13 @@ import { NavLink, Outlet } from "react-router-dom"
 const RootLayout = () => {
 
     const [open, setOpen] = useState(false)
+    
+
   
     return (
         <>
-            <div className="root-layout">
-            <header className="flex md:flex-row flex-col justify-end ml-auto">
+          <div className="root-layout sticky">
+        <header className="flex md:flex-row flex-col justify-end ml-auto ">
           <nav className="p-10 font-mono text-xl">
           <div>
             <ul className="hidden md:flex md:flex-row flex-col md:gap-14 gap-10 list-none">
@@ -17,7 +19,7 @@ const RootLayout = () => {
               <li className='navTag'><NavLink to="project">&#60;Projektek/&#62;</NavLink></li>
               <li className='navTag'><NavLink to="contact">&#60;Elérhetőség/&#62;</NavLink></li>
             </ul>
-            <div className="flex flex-col md:invisible visible gap-5">
+            <div className="flex flex-col md:hidden visible gap-5">
               {!open ? (
                 <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpen(!open)} className="w-12 h-12 hover:rotate-2 cursor-pointer transition-all" viewBox="0 0 24 24"><title>menu-open</title><path d="M21,15.61L19.59,17L14.58,12L19.59,7L21,8.39L17.44,12L21,15.61M3,6H16V8H3V6M3,13V11H13V13H3M3,18V16H16V18H3Z" /></svg>
                 )
@@ -28,10 +30,10 @@ const RootLayout = () => {
               }
               {open && (
               <ul>
-                <li className='navTag animate-[moveIn_0.55s_ease-in-out]'><NavLink to="/">&#60;Kezdőlap/&#62;</NavLink></li>
-                <li className='navTag animate-[moveIn_0.66s_ease-in-out]'><NavLink to="about">&#60;Rólam/&#62;</NavLink></li>
-                <li className='navTag animate-[moveIn_0.77s_ease-in-out]'><NavLink to="project">&#60;Projektek/&#62;</NavLink></li>
-                <li className='navTag animate-[moveIn_0.88s_ease-in-out]'><NavLink to="contact">&#60;Elérhetőség/&#62;</NavLink></li>
+                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.55s_ease-in-out]'><NavLink to="/">&#60;Kezdőlap/&#62;</NavLink></li>
+                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.66s_ease-in-out]'><NavLink to="about">&#60;Rólam/&#62;</NavLink></li>
+                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.77s_ease-in-out]'><NavLink to="project">&#60;Projektek/&#62;</NavLink></li>
+                <li onClick={() => setOpen(!open)} className='navTag animate-[moveIn_0.88s_ease-in-out]'><NavLink to="contact">&#60;Elérhetőség/&#62;</NavLink></li>
               </ul>)
               }        
 
